e298c861c81d875fe61c21a9a82e70a43f0fa434
Tsjippy <ewaldha@hotmail.com>
2026-09-12 07:25:18 +0200

    changed: removed 'tested up to'

--- tsjippy-welcome-message.php
+++ tsjippy-welcome-message.php
@@ -10,10 +10,9 @@ namespace TSJIPPY\WELCOMEMESSAGE;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Tested up to:         7.1
- * Plugin URI:            https://github.com/Tsjippy/welcomemessage
+ * Plugin URI:           https://github.com/Tsjippy/welcomemessage
  * Tested:               7.1
- * TextDomain:            tsjippy
+ * TextDomain:           tsjippy
  * Requires Plugins:    
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
